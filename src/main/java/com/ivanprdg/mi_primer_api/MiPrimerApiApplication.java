e8fdbb31a15f3301bb892188a587bd35ca65284f
Ivanprdg <ivan11cervantes@gmail.com>
2024-07-28 00:31:56 +0200

    Configuracion inicial microservicio practicas

---
+++ src/main/java/com/ivanprdg/mi_primer_api/MiPrimerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ivanprdg.mi_primer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanprdg/mi_primer_api/MiPrimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanprdg.mi_primer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
