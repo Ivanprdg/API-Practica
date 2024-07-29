9a34b43d80f5775312a32d746ebd3016a301973d
Ivanprdg <ivan11cervantes@gmail.com>
2024-07-29 23:10:32 +0200

    Creado Payload Response y aplicado Response Entity al controller

--- src/main/java/com/ivanprdg/controller/ClienteController.java
+++ src/main/java/com/ivanprdg/controller/ClienteController.java
@@ -14,14 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ivanprdg.model.dto.ClienteDto;
 import com.ivanprdg.model.entity.Cliente;
+import com.ivanprdg.model.payload.MensajeResponse;
 import com.ivanprdg.service.ICliente;
 
 @RestController
@@ -35,40 +33,76 @@ public class ClienteController {
 
     @PostMapping("cliente")
     @ResponseStatus(HttpStatus.CREATED)
-    public ClienteDto create(@RequestBody ClienteDto clienteDto) {
+    public ResponseEntity<?> create(@RequestBody ClienteDto clienteDto) {
 
-        Cliente clienteSave = clienteService.save(clienteDto);
-        logger.info("Creating cliente: " + clienteSave);
+        Cliente clienteSave = null;
+        try {
 
-        return ClienteDto.builder()
-        .id_cliente(clienteSave.getId_cliente())
-        .nombre(clienteSave.getNombre())
-        .apellido(clienteSave.getApellido())
-        .correo(clienteSave.getCorreo())
-        .fecha_registro(clienteSave.getFecha_registro())
-        .build();
+            clienteSave = clienteService.save(clienteDto);
+            logger.info("Creating cliente: " + clienteSave);
+
+            clienteDto = ClienteDto.builder()
+            .id_cliente(clienteSave.getId_cliente())
+            .nombre(clienteSave.getNombre())
+            .apellido(clienteSave.getApellido())
+            .correo(clienteSave.getCorreo())
+            .fecha_registro(clienteSave.getFecha_registro())
+            .build();
+
+            return  new ResponseEntity<>(MensajeResponse.builder()
+            .mensaje("Creado correctamente")
+            .object(clienteDto)
+            .build(), HttpStatus.CREATED);
+
+        } catch (DataAccessException exDt) {
+
+            return new ResponseEntity<>(
+                MensajeResponse.builder()
+                .mensaje(exDt.getMessage()).
+                object(null).
+                build()
+                , HttpStatus.INTERNAL_SERVER_ERROR); //Devolvemos un mensaje de error
+        }
     }
 
     @PutMapping("cliente")
     @ResponseStatus(HttpStatus.CREATED)
-    public ClienteDto update(@RequestBody ClienteDto clienteDto) {
+    public ResponseEntity<?> update(@RequestBody ClienteDto clienteDto) {
 
-        Cliente clienteUpdate = clienteService.save(clienteDto);
-        logger.info("Creating cliente: " + clienteUpdate);
+        Cliente clienteUpdate = null;
+        try {
+
+            clienteUpdate = clienteService.save(clienteDto);
+            logger.info("Updating cliente: " + clienteUpdate);
+
+            clienteDto = ClienteDto.builder()
+            .id_cliente(clienteUpdate.getId_cliente())
+            .nombre(clienteUpdate.getNombre())
+            .apellido(clienteUpdate.getApellido())
+            .correo(clienteUpdate.getCorreo())
+            .fecha_registro(clienteUpdate.getFecha_registro())
+            .build();
+
+            return  new ResponseEntity<>(MensajeResponse.builder()
+            .mensaje("Actualizado correctamente")
+            .object(clienteDto)
+            .build(), HttpStatus.CREATED);
+
+        } catch (DataAccessException exDt) {
+
+            return new ResponseEntity<>(
+                MensajeResponse.builder()
+                .mensaje(exDt.getMessage()).
+                object(null).
+                build()
+                , HttpStatus.INTERNAL_SERVER_ERROR); //Devolvemos un mensaje de error
+        }
         
-        return ClienteDto.builder()
-        .id_cliente(clienteUpdate.getId_cliente())
-        .nombre(clienteUpdate.getNombre())
-        .apellido(clienteUpdate.getApellido())
-        .correo(clienteUpdate.getCorreo())
-        .fecha_registro(clienteUpdate.getFecha_registro())
-        .build();
     }
 
     @DeleteMapping("cliente/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) { //ResponseEntity nos permite devolver un mensaje personalizado
 
-        Map<String, Object> response = new HashMap<>(); //Mapa para almacenar la respuesta
         try {
 
             logger.info("Deleting cliente with id: " + id);
@@ -78,25 +112,41 @@ public class ClienteController {
 
         } catch (DataAccessException exDt) {
 
-            response.put("mensaje", exDt.getMessage());
-            response.put("cliente", null);
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR); //Devolvemos un mensaje de error
+            return new ResponseEntity<>(
+                MensajeResponse.builder()
+                .mensaje(exDt.getMessage()).
+                object(null).
+                build()
+                , HttpStatus.INTERNAL_SERVER_ERROR); //Devolvemos un mensaje de error
         }
     }
 
     @GetMapping("cliente/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ClienteDto showById(@PathVariable Integer id) {
+    public ResponseEntity<?> showById(@PathVariable Integer id) {
 
         Cliente cliente = clienteService.findById(id);
         logger.info("Showing cliente with id: " + id);
 
-        return ClienteDto.builder()
-        .id_cliente(cliente.getId_cliente())
-        .nombre(cliente.getNombre())
-        .apellido(cliente.getApellido())
-        .correo(cliente.getCorreo())
-        .fecha_registro(cliente.getFecha_registro())
-        .build();
+        if (cliente == null) {
+            return new ResponseEntity<>(
+                MensajeResponse.builder()
+                .mensaje("El registro que se intenta buscar no existe").
+                object(null).
+                build()
+                , HttpStatus.INTERNAL_SERVER_ERROR); //Devolvemos un mensaje de error
+        }
+
+        return new ResponseEntity<>(
+            MensajeResponse.builder()
+            .mensaje("Consulta exitosa").
+            object(ClienteDto.builder()
+            .id_cliente(cliente.getId_cliente())
+            .nombre(cliente.getNombre())
+            .apellido(cliente.getApellido())
+            .correo(cliente.getCorreo())
+            .fecha_registro(cliente.getFecha_registro())
+            .build())
+            , HttpStatus.OK);
     }
 }
